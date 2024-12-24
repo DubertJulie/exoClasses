c579fc0f96a6c1701c52d31e5e961607f79ef313
DubertJulie <dbt.julie@gmail.com>
2024-12-24 14:39:16 +0100

    Premier commit

---
+++ exercice11/Classes/Salarie.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace exercice11.Classes
+{
+    internal class Salarie
+    {
+
+        // attributs
+        private int _matricule;
+        private string _service;
+        private string _name;
+        private int _salary;
+
+        // propriétés 
+        public int Matricule { get { return _matricule; } set { _matricule = value; } }
+        public string Service { get { return _service; } set { _service = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Salary { get { return _salary; } set { _salary = value; } }
+
+        // constructeur 
+        public Salarie(int matricule, string service, string name, int salary)
+        {
+            Matricule = matricule;
+            Service = service;
+            Name = name;
+            Salary = salary;
+        }
+
+
+
+        public Salarie()
+        {
+            Matricule = 0;
+            Service = "Comptabilité";
+            Name = "John Doe";
+            Salary = 999;
+        }
+
+        public void AfficherSalaire()
+        {
+            Console.WriteLine("Le salaire de " + Name + " est de " + Salary + " euros");
+        }
+
+        public static void AfficherEntreprise(List<Salarie> listeSalariés)
+        {
+            int SalaireTotal = 0;
+
+            foreach (var item in listeSalariés)
+            {
+                SalaireTotal += item.Salary;
+            }
+
+            Console.WriteLine("Le salaire total est de " + SalaireTotal + "euros pour " + listeSalariés.Count + " employés");
+            Console.WriteLine("Le salaire moyen est de " + SalaireTotal / listeSalariés.Count + " euros");
+        }
+
+    }
+}
---
+++ exercice11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+using System.Runtime.InteropServices;
+
+using exercice11;
+using exercice11.Classes;
+
+Salarie Melanie = new Salarie(1, "Espionnage", "Mélanie", 45000);
+Salarie Samuel = new Salarie(2, "Renseignements", "Samuel", 52000);
+Salarie Lisa = new Salarie(3, "Informatique", "Lisa", 70000);
+
+List<Salarie> listeSalariés = new List<Salarie>();
+listeSalariés.Add(Samuel);
+listeSalariés.Add(Lisa);
+listeSalariés.Add(Melanie);
+
+Melanie.AfficherSalaire();
+Samuel.AfficherSalaire();
+Lisa.AfficherSalaire();
+
+
+Salarie.AfficherEntreprise(listeSalariés);
